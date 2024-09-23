2e9adc95007e67f0319363fc0b80a26b08f668c8
SifatSararChistee <sifatsararchistee@gamil.com>
2024-09-23 12:22:16 +0600

    updated btn funtions

---
+++ js/btn-funtions.js
@@ -0,0 +1,14 @@
+document.getElementById("history-btn").addEventListener("click", function () {
+  document.getElementById("history-section").classList.remove("hidden");
+  document.getElementById("card-section").classList.add("hidden");
+  document.getElementById("history-btn").classList.add("bg-lime-300");
+  document.getElementById("history-btn").classList.remove("bg-white");
+  document.getElementById("donation-btn").classList.remove("bg-lime-300");
+});
+document.getElementById("donation-btn").addEventListener("click", function () {
+  document.getElementById("history-section").classList.add("hidden");
+  document.getElementById("card-section").classList.remove("hidden");
+  document.getElementById("history-btn").classList.remove("bg-lime-300");
+  document.getElementById("history-btn").classList.add("bg-white");
+  document.getElementById("donation-btn").classList.add("bg-lime-300");
+});
